086fc162a7368d309338a7ff6108e88ec39e446d
Ramazan Fırat Akdağ <97161858+RamazanAkdag@users.noreply.github.com>
2024-08-08 09:41:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     headers = data[0].keys()
     file = JsonCsvConverter.convertToCsvFile(data, headers)
 
-    data_dir = ".//data"
+    data_dir = "/app/data"
     files = sorted(os.listdir(data_dir), reverse=True)
     print(files)
   
